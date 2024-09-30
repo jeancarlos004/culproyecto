326a374cfe491409c7a209c83e9d72d9f632a0b2
jeancarlos004 <jeanmaldonado834@gmail.con>
2024-09-30 10:00:45 -0500

    Actualizado

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import {  IsEmail, Min, Max, IsInt, MinLength, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+
+    @IsEmail() // Valida que sea un email
+    email?: string;
+    @IsInt()
+    @Max(70,   {message:"la edad no puede ser mayor a 70"})
+    @Min(18,  {message:"la edad deve ser mayor a 18"})
+    age?: number;
+    @MinLength(3, {message:"el nombre deve contener al menos 3 caracteres"})
+    @IsNotEmpty()
+    name:string
+    @IsNotEmpty({ message: 'La dirección no puede estar vacía' })
+    address: string; // Nuevo campo
+  }
+
+    
+    
+   
+    
+
+    
+
+
+    
+
+
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,12 +1,19 @@
-import { Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     name: string;
+
     @Column()
     age: number;
+
+    @Column({ nullable: true })
+    email: string;
+
+    @Column({ nullable: true })
+    address: string;
 }
